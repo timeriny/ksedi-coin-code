aab0c91d1ab81e8c05b9df61e1b1eed0e4c44fcb
tiveriny <timofeibarnaul@gmail.com>
2022-05-10 20:42:38 +0300

    fixed bug  version 0.1

---
+++ miner_config.py
@@ -0,0 +1,9 @@
+"""
+Этот файл нужно изменять до запуска майнера, указывайте правильный адрес во избежании ошибок.
+"""
+
+MINER_ADDRESS = "WALLET ADDRESS"
+
+MINER_NODE_URL = "https://timeriny.github.io/Ksedi-Coin/"
+
+PEER_NODES = []
